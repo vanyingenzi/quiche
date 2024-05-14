452e769b2b757d4e3075d969513508c0a5d5bdd3
vanyingenzi <ingenzivany@gmail.com>
2024-05-14 16:27:34 +0200

    Added a handler when using strace interrupted syscall with poll

--- apps/src/multicore_server.rs
+++ apps/src/multicore_server.rs
@@ -75,7 +75,16 @@ fn server_thread(
             false => Some(Duration::from_millis(10)), // TODO multicore timeout
         };
 
-        poll.poll(&mut events, timeout).unwrap();
+        match poll.poll(&mut events, timeout){
+            Ok(..) => {}, 
+            Err(e) => {
+                if e.kind() == std::io::ErrorKind::Interrupted{ // when using strace
+                    trace!("poll interrupted");
+                } else {
+                    panic!("[Path Thread] error when polling");
+                }
+            }
+        }
 
         'read: loop {
             if events.is_empty() && !continue_write {
